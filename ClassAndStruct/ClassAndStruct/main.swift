69e4ba69e8f2ff48c4a7a7b3a307a7cff66a8568
lyy lee <771408336@qq.com>
2019-08-29 16:45:51 +0800

    class and struct

--- ClassAndStruct/ClassAndStruct/main.swift
+++ ClassAndStruct/ClassAndStruct/main.swift
@@ -12,8 +12,40 @@ struct Date {
 
 
 var date = Date(year: 2019, month: 8, day: 28)
+var date1 = date
+print(Mems.ptr(ofVal: &date))
+print(Mems.ptr(ofVal: &date1))
 
-print(Mems.memStr(ofVal: &date))
-print(MemoryLayout<Date>.size)
-print(MemoryLayout<Date>.stride)
-print(MemoryLayout<Date>.alignment)
+//print(Mems.memStr(ofVal: &date))
+//print(MemoryLayout<Date>.size)
+//print(MemoryLayout<Date>.stride)
+//print(MemoryLayout<Date>.alignment)
+
+class Location {
+    var x: Int = 1
+    var y: Int = 2
+}
+
+var loc = Location()
+//Argument passed to call that takes no arguments
+//let loc = Location(x:1,y:1)
+
+//print(Mems.ptr(ofRef: loc))
+//print(MemoryLayout<Date>.size)      //24
+//print(MemoryLayout<Date>.stride)    //24
+//print(MemoryLayout<Date>.alignment) //8
+
+/*
+ 结构体和类的本质区别：结构体是值类型，类是引用类型
+ 结构体实例对象存放成员变量的值，类的实例对象存放的是对象的地址
+ */
+
+
+/*
+ 值类型赋值给var、let或者给函数传参，是直接将所有内容拷贝一份
+ 在Swift标准库中，为了提升性能，String、Array、Dictionary、Set等采取了Copy On Write的技术：
+ 仅当有“写”操作时，才会真正执行拷贝（a = b，若a、b均不修改，不执行Copy操作）
+ */
+
+
+
--- SwiftPractice.playground/Pages/结构体和类.xcplaygroundpage/Contents.swift
+++ SwiftPractice.playground/Pages/结构体和类.xcplaygroundpage/Contents.swift
@@ -20,9 +20,9 @@ struct Date {
     编译器根据情况，可能会为结构体生成多个初始化器，宗旨是保证所有成员变量都有初始值
 */
 var date = Date(year: 2019, month: 8, day: 28)
-print(MemoryLayout<Date>.size)      //24
-print(MemoryLayout<Date>.stride)    //24
-print(MemoryLayout<Date>.alignment) //8
+//print(MemoryLayout<Date>.size)      //24
+//print(MemoryLayout<Date>.stride)    //24
+//print(MemoryLayout<Date>.alignment) //8
 /*
     结构体date内存内容如下：
     0x00000000000007e3 0x0000000000000008 0x000000000000001c
@@ -71,39 +71,112 @@ var p5 = Point3(x: 1, y: 1)
 
 
 
-//类的定义和结构体类似，但编译器没有为类自动生成可以传入成员值的初始化器
+/*
+    类的定义和结构体类似，但编译器没有为类自动生成可以传入成员值的初始化器
+    类和结构体的本质区别：类是引用类型，结构体是值类型
+    结构体实例对象存放成员变量的值，类的实例对象存放的是对象的地址
+    类的初始化器
+    如果类的所有成员变量定义时指定了初始值，编译器会为类生成无参的初始化器
+ */
+//
 class Location {
-    var x: Int = 0
-    var y: Int = 0
+    var x: Int = 1
+    var y: Int = 2
 }
 
+/* 下面这段代码等同于上面类的定义
+class Location {
+    var x: Int
+    var y: Int
+    init() {
+        x = 1
+        y = 2
+    }
+}
+ */
+
 let loc = Location()
-print(MemoryLayout<Date>.size)      //24
-print(MemoryLayout<Date>.stride)    //24
-print(MemoryLayout<Date>.alignment) //8
+//Argument passed to call that takes no arguments
+//let loc = Location(x:1,y:1)
+
+//print(MemoryLayout<Date>.size)      //24
+//print(MemoryLayout<Date>.stride)    //24
+//print(MemoryLayout<Date>.alignment) //8
+
+
+/*
+    值类型赋值给var、let或者给函数传参，是直接将所有内容拷贝一份
+    类赋值类似制作一个文件的替身，指向的是同一个文件，属于浅拷贝
+ 
+    在Swift标准库中，为了提升性能，String、Array、Dictionary、Set等采取了Copy On Write的技术：
+    仅当有“写”操作时，才会真正执行拷贝（a = b，若a、b均不修改，不执行Copy操作）
+ */
+
+
+/*
+    对象堆空间的申请过程
+    Class.__allocating_init()
+    libswiftCore.dylib:_swift_allocObject_
+    libswiftCore.dylib:swift_slowAlloc
+    libsystem_malloc.dylib::malloc
+    在Mac、iOS中malloc函数分配的内存大小总是16的倍数
+    通过class_getInstanceSize可以得知类对象至少需要占用多少内容
+ */
+
+//print(class_getInstanceSize(Location.self))
+//print(class_getInstanceSize(type(of: loc)))
+
+/*
+    类的成员变量的修改是通过指针找到堆空间的对象内存，覆盖内存中的值
+    值类型成员变量的修改是直接覆盖成员变量的值
+    所以值类型对象用let修饰不能修改成员变量的值，类对象可以
+*/
+
+
+/*
+    枚举、结构体、类都可以定义方法
+    定义在枚举、结构体、类中的函数，称做放法
+    方法、函数都存储在代码段，不占用对象内存
+ */
+
+
+class Person {
+    var name : String = ""
+    var sex: String = ""
+    func personalInformation() -> String {
+        return "name:\(name) \n sex:\(sex)"
+    }
+}
+
+let p = Person()
+p.name = "lyy"
+p.sex = "male"
+p.personalInformation()
 
 
 
+struct Rect {
+    var x = 10
+    var y = 10
+    func isSquare() -> Bool {
+        return x == y
+    }
+}
+
+var rect = Rect()
+rect.y = 20
+rect.isSquare()
 
 
+enum Weather : String {
+    case sunny = "Sunny",rainy = "Rainy",cloudy = "Cloudy"
+    func stateOfWeather() -> String {
+        return "\(rawValue)"
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let weather = Weather.sunny
+weather.stateOfWeather()
 
 
 
