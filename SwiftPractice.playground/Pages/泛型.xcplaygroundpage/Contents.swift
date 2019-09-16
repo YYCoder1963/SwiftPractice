3eb60305b8804725d22a23dcd3815b64a8e985fb
lyy lee <771408336@qq.com>
2019-09-16 15:22:59 +0800

    泛型

--- SwiftPractice.playground/Pages/泛型.xcplaygroundpage/Contents.swift
+++ SwiftPractice.playground/Pages/泛型.xcplaygroundpage/Contents.swift
@@ -91,6 +91,22 @@ protocol Stackable {
     func size() -> Int
 }
 
+class Stack2<E> : Stackable {
+    var elements = [E]()
+    func push(_ element: E) {
+        elements.append(element)
+    }
+    func pop() -> E {
+        return elements.removeLast()
+    }
+    func top() -> E {
+        return elements.last!
+    }
+    func size() -> Int {
+        return elements.count
+    }
+}
+
 class StringStack : Stackable {
     typealias Element = String
     var elements = [String]()
@@ -108,17 +124,142 @@ class StringStack : Stackable {
     }
 }
 
+var ss = StringStack()
+ss.push("lyy")
+ss.pop()
+
+//类型约束
+protocol Runnable {}
+class Person {}
+func swapValues<P: Person & Runnable>(_ a: inout P, _ b: inout P){
+    (a, b) = (b, a)
+}
 
 
 
+protocol Stackable1 {
+    associatedtype Element: Equatable
+}
+
+class Stack3<E: Equatable> : Stackable1 {
+    typealias Element = E
+}
+
+func equal<S1: Stackable1, S2: Stackable1>(_ s1: S1, _ s2: S2) -> Bool
+    where S1.Element == S2.Element, S1.Element : Hashable {
+        return false
+}
+
+var stack10 = Stack3<Int>()
+var stack11 = Stack3<Int>()
+equal(stack10, stack11)
+
+
+//协议类型的注意点
+class Person1 : Runnable {}
+class Car : Runnable {}
+func get(_ type: Int) -> Runnable {
+    if type == 0 {
+        return Person1()
+    }
+    return Car()
+}
+
+
+protocol Runnable1 {
+    associatedtype Speed
+    var speed : Speed { get }
+}
+
+class Dog : Runnable {
+    var speed : Double { return 0.0 }
+}
+
+class Jeep : Runnable {
+    var speed : Int { return 0 }
+}
+
+
+func get1(_ type: Int) -> Runnable {
+    if type == 0 {
+        return Dog()
+    }
+    return Jeep()
+}
 
 
 
+func get<T : Runnable>(_ Type: Int) -> T {
+    if Type == 0 {
+        return Person1() as! T
+    }
+    return Car() as! T
+}
+
+//用some关键字声明一个不透明类型
+//func get2(_ type: Int) -> some Runnable {
+//    return Car()
+//}
 
 
+//some限制只能返回一种类型
+//func get(_ type: Int) -> some Runnable {
+//    if type == 0 {
+//        return Person()
+//    }
+//    return Car()
+//}
 
 
+//some也可以用在属性类型上
+//class Person5 {
+//    var per: some Runnable {
+//        return Dog()
+//    }
+//}
 
+
+/* 可选项的本质是enum类型
+ 
+ public enum Optional<Wrapped> : ExpressibleByNilLiteral {
+    case none
+    case some(Wrapped)
+    public init(_ some: Wrapped)
+ }
+ 
+*/
+
+var age: Int? = 1
+var age1: Optional<Int> = Optional<Int>.some(10)
+var age2: Optional = .some(1)
+var age3 = Optional.some(1)
+var age4 = Optional(10)
+age = nil
+age1 = .none
+
+switch age {
+case let v?:
+    print("some",v)
+case nil:
+    print("none")
+}
+
+switch age1 {
+case let .some(v):
+    print("some",v)
+case .none:
+    print("none")
+}
+
+
+var age_: Int? = 11
+var age5: Int?? = age_
+
+var age6 = Optional.some(Optional.some(1))
+//age6 = .none
+var age7 = age6!
+var age8 = age7!
+print(age6 as Any,age7 as Any,age8)
 
 
 
